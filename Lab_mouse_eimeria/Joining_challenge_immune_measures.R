4cfe9a9cc0abdae459edf1f50afcc9a6065bf890
fayweb <fy.webster@gmail.com>
2021-11-18 15:54:51 +0100

    Working on joining experiment p4

--- Lab_mouse_eimeria/Joining_challenge_immune_measures.R
+++ Lab_mouse_eimeria/Joining_challenge_immune_measures.R
@@ -17,6 +17,31 @@ P4_FACS$EH_ID <- str_replace_all(P4_FACS$EH_ID, "LM_", "LM") #replace string val
 str(P4_FACS)
 colnames(P4_FACS)
 
+#read experimental design P4
+P4_Design <- read.csv("https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/Experimental_design/P4_082020_Eim_DESIGN.csv")
+P4_Design$EH_ID <- str_replace_all(P4_Design$EH_ID, "LM_", "LM") #replace string values in EH_ID Column so that table matches the challenge infections table
+
+#join P4_FACS and P4_Design
+P4_Full <- P4_FACS %>%
+  left_join(P4_Design, by = "EH_ID") 
+#%>%
+ # select(!experiment) %>%
+ # select(!challenge)
+str(P4_Full)
+
+
+#read Experimental design P4
+P4_record <- read.csv("https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/Experiment_results/P4_082020_Eim_record.csv")
+P4_record$EH_ID <- str_replace_all(P4_record$EH_ID, "LM_", "LM") #replace string values in EH_ID Column so that table matches the challenge infections table
+P4_record <-P4_record %>% 
+  rename(label = labels)
+str(P4_record)
+
+#join P4_full with record 
+P4_Full2 <- P4_full %>%
+  right_join(P4_record, by = c("EH_ID", "experiment", "label", "dpi"))
+str(P4_Full2)
+
 #read E57 FAcs
 E57_FACS <- read.csv("https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/Experiment_results/E7_112018_Eim_FACS.csv")
 E57_FACS$EH_ID <- str_replace_all(E57_FACS$EH_ID, "LM_", "LM") #replace string values in EH_ID Column so that table matches the challenge infections table
