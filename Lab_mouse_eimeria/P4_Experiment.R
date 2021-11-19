e6941433f95c74f0349107d660f7eefc16b292df
fayweb <fy.webster@gmail.com>
2021-11-19 16:02:18 +0100

    Created a data frame with the combined base data for the P4 Experiment. Addedt the P4 Experiment to the challenge infections.

--- Lab_mouse_eimeria/P4_Experiment.R
+++ Lab_mouse_eimeria/P4_Experiment.R
@@ -4,74 +4,68 @@ library(ggplot2)
 library(dplyr)
 library(stringr) #to manipulate strings
 library(magrittr)
-
+library(janitor)
 
 #read challenge infections
 Challenge_Infections <- read.csv("https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data_products/Challenge_infections.csv")
-str(Challenge_Infections)
-#show column names
-colnames(Challenge_Infections)
-
-#read facs tables 
-#starting with p4 FACS
-P4_FACS <- read.csv("https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/Experiment_results/P4_082020_Eim_FACS.csv")
-P4_FACS$EH_ID <- str_replace_all(P4_FACS$EH_ID, "LM_", "LM") #replace string values in EH_ID Column so that table matches the challenge infections table
-str(P4_FACS)
-colnames(P4_FACS)
 
 #read experimental design P4
 P4_Design <- read.csv("https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/Experimental_design/P4_082020_Eim_DESIGN.csv")
 P4_Design$EH_ID <- str_replace_all(P4_Design$EH_ID, "LM_", "LM") #replace string values in EH_ID Column so that table matches the challenge infections table
 
-#join P4_FACS and P4_Design
-P4_Full <- P4_FACS %>%
-  left_join(P4_Design, by = "EH_ID") %>%
+#read Experimental record P4
+P4_record <- read.csv("https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/Experiment_results/P4_082020_Eim_record.csv") %>%
   select(!X)
-#%>%
-# select(!experiment) %>%
-# select(!challenge)
-str(P4_Full)
 
-#read Experimental design P4
-P4_record <- read.csv("https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/Experiment_results/P4_082020_Eim_record.csv")
 P4_record$EH_ID <- str_replace_all(P4_record$EH_ID, "LM_", "LM") #replace string values in EH_ID Column so that table matches the challenge infections table
-P4_record <-P4_record %>% 
-  rename(label = labels) %>%
-  select(!X)
-str(P4_record)
 
-#join P4_full with record 
-P4_Full <- P4_record %>%
-  left_join(P4_Full, by = c("EH_ID", "experiment", "label", "dpi"))
+#write a function to create a list of common column names
+names_common <- function(x, y) {
+  intersect(colnames(x), colnames(y))
+}
+
+#write a function to left join 
+join_my_tables <- function (x, y) {
+  x %>%
+    left_join(y, by = c(names_common(x, y)))
+}
+
+#join P4_Design and P4_record
+P4_Des_Rec <- join_my_tables(P4_record, P4_Design)
 
 #add the oocyst counts
-P4_oocysts <- read.csv("https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/Experiment_results/P4_082020_Eim_oocyst.csv") %>% 
-  rename(label = labels) %>%
+P4_oocysts <- read.csv("https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/Experiment_results/P4_082020_Eim_oocyst.csv") %>%
   select(!X)
 
-#join the oocyst counts to the P4_full
-P4_Full <- P4_Full %>%
-  left_join(P4_oocysts, by = c("label", "experiment")) %>%
-  rename(labels = label)
+#join the oocyst counts to the P4_Des_Rec
+P4_Experiment <- join_my_tables(P4_Des_Rec, P4_oocysts)
 
-str(P4_Full)
+#which column names are common
+names_common(P4_Experiment, Challenge_Infections)
 
-#which column names are matching
-colnames(P4_Full) %in% colnames(Challenge_Infections) %>% table
-colnames(Challenge_Infections)
+#compare the columns of the two dataframes
+compare_df_cols(P4_Experiment, Challenge_Infections)
 
-#use the code of Emanuel on challenge infections to add the infection history and infection type variable 
-P4_Full %>% filter(!is.na(P4_Full)) %>%
-  
-  mutate(challenge_infection=ifelse(!experiment%in%"E57",
-                                    challenge_infection,
-                                    ifelse(challenge_infection%in%"E88", "UNI",
-                                           challenge_infection))) %>%
-  ## then correct the infection history
-  mutate(infection_history=paste0(primary_infection, "_",
-                                  challenge_infection)) 
+#create column infection_history
+P4_Experiment %>%
+  mutate(infection_history = paste0(primary_infection, "_",
+         challenge_infection)) -> P4_Experiment
 
+#produce the column infection type
+P4_Experiment %>%
+  mutate(infection_type = case_when(
+    P4_Experiment$infection == "primary" & primary_infection == "UNI" ~ paste0("UNI"),
+    P4_Experiment$infection =="challenge" & challenge_infection == "UNI" ~ paste0("UNI"),
+    P4_Experiment$infection == "primary" ~ paste0("primary_", primary_infection),
+    P4_Experiment$infection == "challenge" ~ paste0("heterologous_", challenge_infection),
+    TRUE ~ "other"
+  )) -> P4_Experiment
 
 #write the table 
-write.csv(P4_Full, "/home/fay/Documents/GitHub/Eimeria-PhD-Project/Lab_mouse_eimeria/Products/P4_Experiment_joined", row.names = FALSE)
+write.csv(P4_Experiment, "/home/fay/Documents/GitHub/Eimeria-PhD-Project/Lab_mouse_eimeria/Products/P4_Experiment", row.names = FALSE)
 
+#bind the P4 experiments to the challenge infections
+Challenge_infections_with_p4 <- bind_rows(Challenge_Infections, P4_Experiment)
+
+#write the combination table
+write.csv(Challenge_infections_with_p4, "/home/fay/Documents/GitHub/Eimeria-PhD-Project/Lab_mouse_eimeria/Products/Challenge_p4", row.names = FALSE)
